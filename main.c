29b1448abf6d991fc7f3c65e56ec96ebf7503820
NewrootParrotOS <84894136+NewrootParrotOS@users.noreply.github.com>
2021-05-27 01:09:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-  char name;
+  char name, task;
   printf("Hello World\n");
   printf("What is yoour name?\t");
   scanf("%s", &name);
   printf("Hello %f what can I do for you?");
+  scanf("%s" &task);
+  printf("Sure, i will do that for you");
   return 0;
 }
